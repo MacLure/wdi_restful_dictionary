8bfe6469a059d139f0370aa63fe0e822506efe77
Malcolm <malcolm@Malcolms-MacBook-Pro.local>
2018-12-04 16:05:41 -0500

    complete the assignment

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,12 +1,29 @@
 class EntriesController < ApplicationController
 
     def index
-        render :index
+        # render :index
     end
 
     def show
         @entry = Entry.find(params[:id])
-        render :show
     end
 
+    def new
+    end
+
+    def create
+        redirect_to entries_url
+    end
+
+    def edit
+    end
+
+    def update
+        redirect_to entry_url(params[:id])
+    end
+
+    def destroy
+        redirect_to entries_url
+    end
+    
 end
